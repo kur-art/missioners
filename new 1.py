7d91c9edba9fbe803581c3372d9ff4f881a28b5f
kur-art <135021395+kur-art@users.noreply.github.com>
2023-06-06 12:25:59 +0300

    Add files via upload

---
+++ new 1.py
@@ -0,0 +1,23 @@
+<html><style="background-color:red"}>   
+<title>purpose ressurecting missioners 
+</title>
+<Em{"background-color: red"}>PURPOSE RESSURECTING MISSIONERS
+<head><h1 style="font-family:georgia;font-style:italics;color:purple;text-align:center"}>official ministry website</h1>
+
+</head>
+	
+
+<h2>
+		<h3><h3 style="font-weight:bold;text-align:center;color:blue">ABOUT</h3>
+		<h4><h4 style="font-weight:bold;text-align:center;color:red">GOALS</h4>
+        <h5><h5 style="font-weight:bold;text-align:center;color:yellow">FEEDBACK</h5>
+		<h6><h6 style="font-style:italics;font-weight:bold;text-align:center;color:green">CONTACTS</h6>
+	</h2>
+
+
+
+its about time we begin harvesting souls across nations.the mission vision is to bring as much souls to christ kingdom.the organization main objective is togetherness in christ,spreading the gospel and bringing spritual growth to believers
+
+
+
+</html>
